54b465b1301f5e2deb698e5adc27e96742c24b39
Fernando <fercho722@gmail>
2021-11-04 01:22:09 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/EstudianteController.cs
@@ -0,0 +1,87 @@
+﻿using EsrudiantesApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EsrudiantesApp.Controllers
+{
+    public class EstudianteController : Controller
+    {
+        const string nombreSesion = "ListadoEstudiantes";
+        // GET: Estudiante
+        public ActionResult Index()
+        {
+            //Validar si es la primera vez que se inicia la app
+            if (Session[nombreSesion] == null)
+            {
+                var listadoEstudiantes = ObtenerEstudiante();
+                Session[nombreSesion] = listadoEstudiantes;
+                return View(listadoEstudiantes);
+            }
+            else
+            {
+                var listadoEstudiantes = (List<Estudiante>)Session[nombreSesion];
+
+                return View(listadoEstudiantes.OrderBy(x => x.Id).ToList());
+            }
+                
+            
+        }
+        [HttpGet]
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Nuevo(Estudiante estudiante)
+        {
+            var listadoEstudiantes = (List<Estudiante>)Session[nombreSesion];
+            listadoEstudiantes.Add(estudiante);
+            Session[nombreSesion] = listadoEstudiantes;
+            return RedirectToAction("Index");
+        }
+
+        private List<Estudiante> ObtenerEstudiante()
+        {
+            var listado = new List<Estudiante>();
+            listado.Add(new Estudiante { Id = 1, Nombres = "Abigail", Apellidos = "Fernandez", Cursos = "Aleman", Intereses = "Ingenieria" });
+            listado.Add(new Estudiante { Id = 2, Nombres = "Martin", Apellidos = "Fernandez", Cursos = "Programacion", Intereses = "Tecnologia" });
+            listado.Add(new Estudiante { Id = 3, Nombres = "Yeraldin", Apellidos = "Guzman", Cursos = "Ingles", Intereses = "Viajes" });
+            listado.Add(new Estudiante { Id = 4, Nombres = "Flor", Apellidos = "Muñoz", Cursos = "Office", Intereses = "Descanso" });
+            listado.Add(new Estudiante { Id = 5, Nombres = "Fernando", Apellidos = "Fernandez", Cursos = "Ingles", Intereses = "Programacion" });
+            listado.Add(new Estudiante { Id = 6, Nombres = "Isaac", Apellidos = "Moya", Cursos = "Tecnica vocal", Intereses = "Natacion" });
+
+            return listado;
+        }
+        [HttpGet]
+        public ActionResult Actualizar(int id)
+        {
+            var listadoEstudiantes = (List<Estudiante>)Session[nombreSesion];
+            var estudiante = listadoEstudiantes.FirstOrDefault(x => x.Id == id);
+            return View(estudiante);
+        }
+        [HttpPost]
+        public ActionResult Actualizar(Estudiante estudiante)
+        {
+            var listadoEstudiantes = (List<Estudiante>)Session[nombreSesion];
+            var estudianteActualizar = listadoEstudiantes.FirstOrDefault(x => x.Id == estudiante.Id);
+            listadoEstudiantes.Remove(estudianteActualizar);
+            listadoEstudiantes.Add(estudiante);
+          
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Eliminar (int id)
+        {
+            var listadoEstudiantes = (List<Estudiante>)Session[nombreSesion];
+            var estudianteEliminar = listadoEstudiantes.FirstOrDefault(x => x.Id == id);
+            listadoEstudiantes.Remove(estudianteEliminar);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Estudiante.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EsrudiantesApp.Models
+{
+    public class Estudiante
+    {
+        public int Id { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Cursos { get; set; }
+        public string Intereses { get; set; }
+    }
+}
